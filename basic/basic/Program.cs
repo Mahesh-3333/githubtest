f4da659c0a891312cb24555389178f2459e2c466
Mahesh-3333 <mdharme33@gmail.com>
2022-01-16 10:31:14 +0530

    html first comment

---
+++ basic/basic/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
